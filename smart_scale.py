1320ab4d9a9cd2c1cdb1031b23fb01d53af7fc74
Enoch Y. Chau <eychau1@uci.edu>
2019-10-26 10:04:57 -0700

    final firmware

--- smart_scale.py
+++ smart_scale.py
@@ -6,34 +6,40 @@ import RPi.GPIO as GPIO
 from hx711 import HX711
 from RPLCD.gpio import CharLCD
 
-import multitasking
-import signal
-# kill all tasks on ctrl-c
-signal.signal(signal.SIGINT, multitasking.killall)
+#threading
+import threading
 
 #hx711 reference value
 referenceUnit = -278
 
 #button setup
 button_pin = 8 #GPIO8
-button_pin2 = 7 #GPIO7
+# button_pin2 = 7 #GPIO7
 
-def cleanAndExit():
-    lcd.clear()
-    print("Cleaning...")
-    GPIO.cleanup()
-    print("Bye!")
-    sys.exit()
+val = 0
 
-@multitasking.task
-def tare_button(hx, lcd):
-    if GPIO.input(button_pin) == GPIO.HIGH:
-        print("tare button pushed")
-        hx.tare()
+def tare_task(hx,lock,run_event):
+    while run_event.is_set():
+        if GPIO.input(button_pin) == GPIO.HIGH:
+            print("tare button pushed")
+            lock.acquire()
+            val = 0
+            hx.tare()
+            hx.power_down()
+            hx.power_up()
+            lock.release()
+
+def weigh_task(hx, lcd,run_event):
+    while run_event.is_set():
+        lock.acquire()
+        val = hx.get_weight(5)
+        val = round(val,2)
+        print(str(val) + " g")
         lcd.clear()
-        lcd.write_string("0 g")
+        lcd.write_string(str(val) + " g")
         hx.power_down()
         hx.power_up()
+        lock.release()
         time.sleep(0.1)
 
 if __name__ == "__main__":
@@ -50,24 +56,31 @@ if __name__ == "__main__":
     #init tare button
     GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     #init weigh button
-    GPIO.setup(button_pin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # GPIO.setup(button_pin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     print("Tare done! Add weight now...")
 
-    while True:
-        try:
-            tare_button(hx, lcd)
-            # if GPIO.input(button_pin2) == GPIO.HIGH:
-            val = hx.get_weight(5)
-            val = round(val,2)
-            print(str(val) + " g")
-            lcd.clear()
-            lcd.write_string(str(val) + " g")
-            hx.power_down()
-            hx.power_up()
-            time.sleep(0.1)
+    lock = threading.Lock()
 
+    run_event = threading.Event()
+    run_event.set()
+    # creating threads
+    t1 = threading.Thread(target=tare_task, args=(hx,lock,run_event))
+    t2 = threading.Thread(target=weigh_task, args=(hx,lcd,run_event))
+    t1.start()
+    t2.start()
+    try:
+        while True:
+            time.sleep(.1)
+    except (KeyboardInterrupt, SystemExit):
+        run_event.clear()
+        t1.join()
+        t2.join()
+        print("threads joined")
+        lcd.clear()
+        print("lcd cleared")
+        GPIO.cleanup()
+        print("pins cleaned")
 
-
-        except (KeyboardInterrupt, SystemExit):
-            cleanAndExit()
+        print("Bye!")
+        sys.exit()
